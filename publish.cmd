6484df582b452fa60da4b08d0acf7e58a528f865
Takaya Kawano <takawano@microsoft.com>
2019-05-22 15:15:39 +0900

    UPdated

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=sampledevopsbot-Web-Deploy.pubxml -p:Password=kcQcgKYTyQxxd6vsjoywFxQXfH8fLEcbpZ8BNowbns7m5XbykmYodWm4Bnbo
+
